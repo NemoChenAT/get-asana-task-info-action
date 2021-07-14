02c925bf0428e6c6f6f650f99c06b5f5210937ce
NemoChen <nemo.chen@amazingtalker.com>
2021-07-15 00:41:41 +0800

    Fix the input & output parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-taskInfo=$(python3 /get_asana_task_info.py $1 $2 $3 2>&1)
-echo ::set-output name=taskInfo::$taskInfo
+task_info=$(python3 /get_asana_task_info.py $1 $2 $3 2>&1)
+echo ::set-output name=task_info::$task_info
